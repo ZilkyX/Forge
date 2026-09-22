090266e6baab40deed4da8c9df7fad4698f94cbb
huertasjoey <165572859+huertasjoey@users.noreply.github.com>
2026-09-23 00:31:02 +0800

    navbar unfinished

--- web/src/pages/landing/components/Navbar.tsx
+++ web/src/pages/landing/components/Navbar.tsx
@@ -1,7 +1,22 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+
+    <nav className="flex items-center justify-between px-8 py-5">
+
+      <div><img src="./logo/logo.jpg" alt="" /><h1 className="text-2x1 font-bold">FORGE</h1></div>
+
+      <div className="flex gap-8 items-center">
+        <a href="#" className="text-sm font-medium">HOME</a>
+
+        <a href="#" className="text-sm font-medium">ABOUT US</a>
+
+        <a href="#" className="text-sm font-medium">PROGRAM</a>
+
+        <button className="rounded-lg bg-lime-400 px-5 py-4 text-sm font-black">SIGN UP</button>
+      </div>
+    </nav>
+      
   )
 }
 
